529fe41b00f90e50264155eaccbee560f310df44
Alex Esquerdo <alex.esquerdo@the-cocktail.com>
2024-05-22 11:07:33 +0200

    feat: add inital configuration with its dependencies

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Marvel-App
-Android app repository integrates Hillt DI, Clean Architecture with MVVM, and Flows for reactive programming. This setup ensures efficient dependency management, modularity, and responsiveness in app development.
+# 🤖 Marvel App APP ANDROID
+## What is this repository for?
+
+Android app repository integrates Hilt DI, Clean Architecture with MVVM, and RxJava/RxKotlin for reactive programming. This setup ensures efficient dependency management, modularity, and responsiveness in app development.
+
+## Technology Stack
+
+|                | **Name** | **Version** |  
+|----------------|----------|-------------|  
+| **Lenguage**   | Kotlin   | 1.9.0       |  
+| **Build Tool** | Gradle   | 8.3.1       |  
+
+
+## 🛠 Setup & Installation
+* This app is and app which uses the [Marvel Developer](https://developer.marvel.com/documentation/getting_started) to be able to show trending gifs.
+* To be able to execute this app it will have the necessity to create an account and obtain an API KEY.
+* Finally on local.properties add this lines:
+```  
+public_key=<Your Public Api Key>
+private_key=<Your Private Api Key>    
+```  
+
+## Libraries used on this project
+- [Kotlin](https://kotlinlang.org/)
+- [JetpackCompose](https://developer.android.com/jetpack/compose)
+- [Hilt](https://developer.android.com/training/dependency-injection/hilt-android)
+- [ViewModel](https://developer.android.com/topic/libraries/architecture/viewmodel)
+- [Coroutines](https://github.com/Kotlin/kotlinx.coroutines)
+- [Flow](https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow/)
+- [Material Design 3](https://developer.android.com/jetpack/compose/designsystems/material3)
+- [KSP](https://github.com/google/ksp)
+- [JUnit](https://developer.android.com/training/testing/local-tests)
+- [Mockk](https://mockk.io)
+- [Secrets Gradle Plugin for Android](https://github.com/google/secrets-gradle-plugin)
---
+++ app/build.gradle.kts
@@ -0,0 +1,105 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.secrets)
+    alias(libs.plugins.hilt)
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.example.marvel_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.marvel_app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.11"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
+val localProperties = gradleLocalProperties(rootDir, providers)
+val publicKey: String = localProperties.getProperty("public_key")
+val privateKey: String = localProperties.getProperty("private_key")
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.hilt.android)
+    implementation(libs.logging.interceptor)
+    implementation(libs.retrofit)
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    implementation(libs.converter.moshi)
+    implementation(libs.arrow.core)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.gif)
+    implementation(libs.lottie.compose)
+    ksp(libs.androidx.room.compiler)
+    ksp(libs.hilt.android.compiler)
+    ksp(libs.moshi.kotlin.codegen)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.mockwebserver)
+    testImplementation(libs.test.junit5.params)
+    testImplementation(libs.test.junit5.api)
+    testImplementation(libs.test.junit5.engine)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
